02e70811360ea189b4b210816c8508cda6e757a6
Mayankmsarwa <mayanksarwa204@gmail.com>
2026-04-19 17:17:42 +0530

    moved all the files to  01_basics

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,36 @@
+// DATES
+
+
+// let myDate = new Date();
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate);
+
+// let CreatedDate = new Date(2023, 0 ,23)
+// console.log(CreatedDate)
+
+// let CreatedDate = new Date(2023, 0 ,23,5,3)
+// console.log(CreatedDate.toLocaleString())
+
+// let CreatedDate = new Date("01-14-2026")
+// console.log(CreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(CreatedDate.getTime()) //1768329000000
+
+console.log(Math.floor(Date.now()/1000))
+
+
+// let newDate = new Date("04-20-2026")
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay()) //sun-0, mon-1, tue-2, wed-3, thu-4, fri-5, sat-6
+
+// //${newDate.getDay()} and the time
+
+// console.log(newDate.toLocaleString('default',{
+//     weekday: "long",
+
+// }))
